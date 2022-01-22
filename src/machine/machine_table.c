09f0ea3ff1930e4511e0f00a5fa941baafea83e2
AstroMadson <98224233+AstroMadson@users.noreply.github.com>
2022-01-22 10:49:02 -0500

    Update machine_table.c

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -536,10 +536,11 @@ const machine_t machines[] = {
     /* This has the Phoenix MultiKey KBC firmware.
        This is basically an Intel Premiere/PCI II with a fancier POST screen. */
     { "[i430NX] AMBRA DP90 PCI",		"ambradp90",		MACHINE_TYPE_SOCKET5,		CPU_PKG_SOCKET5_7, 0, 50000000, 66666667, 3380, 3520, 1.5, 1.5,							MACHINE_PCI | MACHINE_BUS_PS2 | MACHINE_IDE_DUAL,				 2048, 131072, 2048, 127,	    machine_at_ambradp90_init, NULL			},
+    /* This has the Phoenix MultiKey KBC firmware.
+       This is basically an Intel Premiere/PCI II with a fancier POST screen. */
+    { "[i430NX] Packard Bell PB540",		"pb540",		MACHINE_TYPE_SOCKET5,		CPU_PKG_SOCKET5_7, 0, 60000000, 66666667, 3520, 3520, 1.5, 1.5,							MACHINE_PCI | MACHINE_BUS_PS2 | MACHINE_IDE_DUAL | MACHINE_VIDEO,		 8192, 139264, 2048, 127,	        machine_at_pb540_init, at_pb540_get_device	},
     /* Has AMI MegaKey KBC firmware. */
     { "[i430NX] Gigabyte GA-586IP",		"430nx",		MACHINE_TYPE_SOCKET5,		CPU_PKG_SOCKET5_7, 0, 60000000, 66666667, 3520, 3520, 1.5, 1.5,							MACHINE_PCI | MACHINE_BUS_PS2 | MACHINE_IDE_DUAL,				 2048, 131072, 2048, 127,		machine_at_430nx_init, NULL			},
-    /* This has the Phoenix MultiKey KBC firmware. */
-    { "[i430NX] Packard Bell PB540",		"pb540",		MACHINE_TYPE_SOCKET5,		CPU_PKG_SOCKET5_7, 0, 60000000, 66666667, 3520, 3520, 1.5, 1.5,							MACHINE_PCI | MACHINE_BUS_PS2 | MACHINE_IDE_DUAL | MACHINE_VIDEO,		 8192, 139264, 2048, 127,	        machine_at_pb540_init, at_pb540_get_device	},
 	
     /* 430FX */
     /* Uses an ACER/NEC 90M002A (UPD82C42C, 8042 clone) with unknown firmware (V5.0). */
